b23e3555ae281a1999c0ac86c34cb6bc9e90e910
v-jzingh <v-jzingh@VISUAL-S>
2016-10-19 16:19:23 +0800

    Models Ok but error create controller with EF

---
+++ Praise/Praise/DAL/MemberInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Praise.Models;
+
+
+namespace Praise.DAL
+{
+    public class MemberInitializer:System.Data.Entity.
+        DropCreateDatabaseIfModelChanges<PraiseContext>
+    {
+        protected override void Seed(PraiseContext context)
+        {
+            var accounts = new List<AccountInfo>
+            {
+                new AccountInfo {Name="Jing Zhang" },
+                new AccountInfo {Name="Meng Liang" },
+                new AccountInfo {Name="Alan Wang" },
+                new AccountInfo {Name="Abby Qu" },
+                new AccountInfo {Name="Anna Guo" },
+                new AccountInfo {Name="Ben Dong" },
+                new AccountInfo {Name="Fred Gao" },
+                new AccountInfo {Name="Hebe Shi" },
+                new AccountInfo {Name="Jack Zhou" },
+                new AccountInfo {Name="Ketty Yu" },
+                new AccountInfo {Name="Laney Li" }
+            };
+            accounts.ForEach(s => context.Accounts.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Praise/Praise/Models/Detail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Praise.Models
+{
+    public class Detail
+    {
+        public string Name { get; set; }
+        DateTime Date { get; set; }
+        string Desc { get; set; }
+        string ToWho { get; set; }
+
+        public virtual ICollection<AccountInfo> AccountInfos { get; set; }
+
+    }
+}
